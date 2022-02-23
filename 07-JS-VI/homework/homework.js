dac2032a5fed8ebbc781de8d1cc7e609cca75e75
GuerreroEnier <guerreroenier@gmail.com>
2022-02-23 16:45:46 -0500

    solucion hw 07-JS-VI 100%

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let cadena = nombre[0];
+  return cadena.toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +26,31 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = 0;
+  //Con for
+  /* for(let i = 0; i<numeros.length; i++){
+    suma += numeros[i];
+  } */
+  //Con reduce
+  /* suma = numeros.reduce((a,b) => a+b); */
+  //Con forEach
+  numeros.forEach(elem =>{
+    suma+=elem;
+  });
+  
+  
+  cb(suma);
+
+  
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach((elemento) => {
+    cb(elemento);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +58,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let nuevoArray = array.map(elem =>  cb(elem));
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let nuevo = array.filter(con => {
+    if(con[0] === 'a')
+      return con;
+  });
+
+  return nuevo;
 }
 
 // No modificar nada debajo de esta línea
